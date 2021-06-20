03e77bb94dcdd8823d24d7bf05d6fc47ad3b08fe
Luis Pimentel <lpimentel3@gatech.edu>
2021-06-20 15:16:10 -0600

    removes build dir from git

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luis/ercr_ws/src/ercr_gazebo
-# Build directory: /home/luis/ercr_ws/src/ercr_gazebo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("msgs")
